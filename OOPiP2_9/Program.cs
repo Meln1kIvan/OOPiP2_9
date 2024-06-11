55078dc2d5e8bc12261361709cffb0decbd331d3
Meln1kIvan <149098836+Meln1kIvan@users.noreply.github.com>
2024-06-11 23:08:58 +0300

    Add files via upload

---
+++ OOPiP2_9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPiP2_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "D:\\2 курс\\OOPiP2_9\\OOPiP2_9\\bin\\Debug\\paper.txt";
+
+            double totalSum = File.ReadLines(fileName).Select(line => line.Split(' ').Select(int.Parse).Select(num => { double avg = (double)num; Console.WriteLine($"Содержимое строки: {avg}"); return avg; }).Sum()).Sum(); Console.WriteLine($"Сумма всех чисел: {totalSum}");
+        }
+    }
+}
